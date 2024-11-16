45120af6a2f2c94cff0aeeb6600fecabbe65972b
ngeran <ngeran@gmail.com>
2024-11-16 15:06:56 -0500

    first commit

---
+++ themes/vector/tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+   content: [
+     /* css from assets folder */
+     "assets/css/*.css",
+     /* relevant files from the blog + theme */
+     "../../content/**/*.{html,md}",
+     "../../layouts/**/*.html",
+     /* relevant files from the theme */
+     "./layouts/**/*.html",
+     /* also pick nested css from theme */
+     "../../assets/css/*.css",
+   ],
+   theme: {
+    extend: {
+       backgroundImage: {
+         'hero': "url('/hero.webp')",
+         'hero-2': "url('/idea-2.jpg')",
+      },
+     },
+   },
+   plugins: [],
+ }
+ 
